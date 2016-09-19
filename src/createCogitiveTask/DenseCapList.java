127d6810c04c4bbb40dc0e800bdedbb572fdaa2a
Takeshi <Takeshi@192.168.1.7>
2016-09-19 22:41:54 +0900

    DenseCap

---
+++ src/createCogitiveTask/DenseCapList.java
@@ -0,0 +1,16 @@
+package createCogitiveTask;
+
+import java.util.List;
+
+public class DenseCapList {
+
+	private List<DenseCap> densecaps;
+
+	public void setDensecaps(List<DenseCap> densecaps) {
+		this.densecaps = densecaps;
+	}
+
+	public List<DenseCap> getDensecaps() {
+		return densecaps;
+	}
+}
