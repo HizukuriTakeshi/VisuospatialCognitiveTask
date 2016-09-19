2a551547395e01ed17ffa8b86916d697cefa5850
hizukuri <hizukuri@hizukuri>
2016-09-19 17:36:12 +0900

    hoge

--- src/createCogitiveTask/HogeList.java
+++ src/createCogitiveTask/HogeList.java
@@ -4,17 +4,10 @@ import java.util.List;
 
 public class HogeList {
 
-    private String id;
+    
 
     private List<Hoge> hoges;
 
-    public void setId(final String id) {
-        this.id = id;
-    }
-
-    public String getId() {
-        return id;
-    }
 
     public void setHoges(final List<Hoge> hoges) {
         this.hoges = hoges;
--- src/createCogitiveTask/JsonicSample.java
+++ src/createCogitiveTask/JsonicSample.java
@@ -8,12 +8,11 @@ public class JsonicSample {
 
      
      // 配列を含むJSON形式のデータを用意
-     final String hoges = "{\"id\":\"10\",\"hoges\":[{\"name\":\"test\",\"age\":1},{\"name\":\"sample2\",\"age\":2}]}";
+     final String hoges = "{\"hoges\":[{\"name\":\"test\",\"age\":1},{\"name\":\"sample2\",\"age\":2}]}";
 
      final HogeList hogeList = JSON.decode(hoges, HogeList.class);
 
      // hogesのようにHogeの形式の配列を持つJSONを、HogeクラスのListをもつクラスにデコードすることが出来ます
-     System.out.println("hogeList.id:" + hogeList.getId());
      for (final Hoge tmp : hogeList.getHoges()) {
          System.out.println("tmp.name:" + tmp.getName());
          System.out.println("tmp.age:" + tmp.getAge());
