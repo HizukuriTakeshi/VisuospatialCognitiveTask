4528e0d80238a14e5153eea8d94a894f14bd5e56
Takeshi <Takeshi@192.168.1.7>
2016-09-19 22:14:40 +0900

    Home

--- src/createCogitiveTask/JsonicSample.java
+++ src/createCogitiveTask/JsonicSample.java
@@ -1,21 +1,39 @@
 package createCogitiveTask;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 //JSONICをインポート
 import net.arnx.jsonic.JSON;
+import net.arnx.jsonic.JSONException;
 
 public class JsonicSample {
  public static void main(String[] args) {
 
      
      // 配列を含むJSON形式のデータを用意
-     final String hoges = "{\"hoges\":[{\"name\":\"test\",\"age\":1},{\"name\":\"sample2\",\"age\":2}]}";
+     
+     //final HogeList hogeList = JSON.decode(hoges, HogeList.class);
 
-     final HogeList hogeList = JSON.decode(hoges, HogeList.class);
-
-     // hogesのようにHogeの形式の配列を持つJSONを、HogeクラスのListをもつクラスにデコードすることが出来ます
-     for (final Hoge tmp : hogeList.getHoges()) {
-         System.out.println("tmp.name:" + tmp.getName());
-         System.out.println("tmp.age:" + tmp.getAge());
-     }
+     try {
+		HogeList hogeList = JSON.decode(new FileReader("src/results.json"), HogeList.class);
+		 
+	     for (final Hoge tmp : hogeList.getHoges()) {
+	         System.out.println("tmp.name:" + tmp.getName());
+	         System.out.println("tmp.age:" + tmp.getAge());
+	}
+	     
+     } catch (JSONException e) {
+		// TODO 自動生成された catch ブロック
+		e.printStackTrace();
+	} catch (FileNotFoundException e) {
+		// TODO 自動生成された catch ブロック
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO 自動生成された catch ブロック
+		e.printStackTrace();
+	}
+    
  } 
 }
