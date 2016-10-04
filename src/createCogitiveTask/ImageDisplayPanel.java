c3a2882217fec2847a9e7031e42447eaeb0b10a1
HizukuriTakeshi <hizukuri@cmc.ss.is.nagoya-u.ac.jp>
2016-10-04 14:45:25 +0900

    ImageDisplayPanel add

---
+++ src/createCogitiveTask/ImageDisplayPanel.java
@@ -0,0 +1,14 @@
+package createCogitiveTask;
+
+import javax.swing.JPanel;
+
+public class ImageDisplayPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public ImageDisplayPanel() {
+
+	}
+
+}
